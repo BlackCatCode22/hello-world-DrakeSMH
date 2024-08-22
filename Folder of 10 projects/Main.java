1d03213ce838357498d7a49223ce64108bffff23
Drake Parker <drakeparker782@gmail.com>
2024-08-22 10:50:15 -0700

    Add files via upload

---
+++ Folder of 10 projects/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n\nWhy did the toilet paper cross the road");
+        System.out.println("It got stuck in a crack?");
+    }
+}
