f0303b07ef1b75f56dc5e2460772f67b47134e3c
Drake Parker <drakeparker782@gmail.com>
2024-08-25 20:25:42 -0700

    Add files via upload

---
+++ Folder of 10 projects/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n\nI went to the doctors today!");
+        System.out.println("Turns out I have cancer in my toenail");
+    }
+}
