a30ac465c2018b505660c416cb16e865df7d47be
Drake Parker <drakeparker782@gmail.com>
2024-08-25 20:03:38 -0700

    Add files via upload

---
+++ Folder of 10 projects/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n\nHello Stranger I hope you are having a good day today!");
+        System.out.println("Just kidding.");
+    }
+}
